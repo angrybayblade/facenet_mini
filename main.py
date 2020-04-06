c1c0b311e005350bcceb2877b0ec966c6d756ddd
Viraj Patel <vptl185@gmail.com>
2020-04-07 05:12:57 +0530

    generated embeddings for net 128

--- main.py
+++ main.py
@@ -1,50 +1,29 @@
 import cv2
 import numpy as np 
 
-from tensorflow.compat.v1 import keras
-from tensorflow.compat.v1.keras import Sequential
-from tensorflow.compat.v1.keras.layers import  Input,Conv2D,Flatten,Dense
-from tensorflow.compat.v1.keras.models import Model
-from tensorflow.compat.v1.keras import backend as K
+from net import Net128
 
-
-def getModel():
-    inputs = Input(shape=(64,64,3))
-    conv = Conv2D(8,4,activation="relu",input_shape=(64,64,3))(inputs)
-    conv = Conv2D(16,3,activation="relu",)(conv)
-    conv = Conv2D(32,2,activation="relu",)(conv)
-    flat = Flatten()(conv)
-    dense = Dense(128,activation="relu")(flat)
-    out = Dense(128)(dense)
-    embedding = Model(inputs,out)
-    embedding.load_weights("./lfw/emb.h5")
-    return embedding
 
 haar = cv2.CascadeClassifier()
 haar.load("./haarcascade_frontalface_default.xml")
-base_img = cv2.imread("./images/user.jpg")
-x,y,w,h = haar.detectMultiScale(base_img.mean(axis=2).astype(np.uint8))[0]
-base_img = cv2.resize(base_img[y:y+h,x:x+w],(64,64)).reshape(1,64,64,3)
 
-model = getModel()
-base_enb = model.predict(base_img)
-
-def score(values): 
-    return np.mean(np.square(base_enb - values))
+base_img = cv2.resize(cv2.imread("./images/user.jpg"),(128,128))
+net = Net128(base_img,"./lfw/128_by_128_emb.h5")
 
 def main():
     cap = cv2.VideoCapture(0)
 
     while True:
         _,frame = cap.read()
+        frame = cv2.medianBlur(frame,3)
         gray  = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
         faces = haar.detectMultiScale(gray)
 
         for x,y,w,h in faces:
-            face = cv2.resize(frame[y:y+h,x:x+w],(64,64)).reshape(1,64,64,3)
-            embeddings = model.predict(face)
+            face = cv2.resize(frame[y:y+h,x:x+w],(128,128))
+            score = net(face)
             frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),3)
-            frame = cv2.putText(frame,str(score(embeddings)),(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255))
+            frame = cv2.putText(frame,str(score),(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255))
 
         cv2.imshow("FRAME",frame)
         if cv2.waitKey(27) & 0xFF == ord('q'):
--- train.py
+++ train.py
@@ -4,25 +4,28 @@ haar = cv2.CascadeClassifier()
 haar.load("./haarcascade_frontalface_default.xml")
 
 
+x,y,w,h = None,None,None,None
+
 def main():
     cap = cv2.VideoCapture(0)
 
     while True:
         _,frame = cap.read()
+        frame = cv2.medianBlur(frame,3)
         img = frame.copy()
         gray = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
         faces = haar.detectMultiScale(gray)
 
         for x,y,w,h in faces:
             frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),2)
+
         cv2.imshow("faces",frame)
         key = cv2.waitKey(1) & 0xFF
-
 
         if key  == ord('q'):
             break
         elif key == ord('s'):
-            cv2.imwrite("./images/user.jpg",img)
+            cv2.imwrite("./images/user.jpg",img[y:y+h,x:x+h])
             break
 
     cap.release()
