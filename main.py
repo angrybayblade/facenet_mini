bc40a0f5201d80580e67660dfb8b4393c88a1c82
Viraj Patel <vptl185@gmail.com>
2020-04-09 18:11:08 +0530

    generated 64 by 64 embeddings

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import cv2
 import numpy as np 
 
-from net import Net128
+from net import Net128,Net64
 
 
 haar = cv2.CascadeClassifier()
 haar.load("./haarcascade_frontalface_default.xml")
 
-base_img = cv2.resize(cv2.imread("./images/user.jpg"),(128,128))
-net = Net128(base_img,"./lfw/128_by_128_emb.h5")
+base_img = cv2.resize(cv2.imread("./images/user.jpg"),(64,64))
+net = Net64(base_img,"./lfw/64_by_64_emb.h5")
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -20,7 +20,7 @@ def main():
         faces = haar.detectMultiScale(gray)
 
         for x,y,w,h in faces:
-            face = cv2.resize(frame[y:y+h,x:x+w],(128,128))
+            face = cv2.resize(frame[y:y+h,x:x+w],(64,64))
             score = net(face)
             frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),3)
             frame = cv2.putText(frame,str(score),(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255))
--- net.py
+++ net.py
@@ -35,3 +35,27 @@ class Net128:
     def __call__(self,inputs):
         assert len(inputs.shape) > 2,"Input Must Have Dimentions Greater Then 2"
         return self.model.predict(inputs.reshape(-1,128,128,3))
+
+
+class Net64:
+    def __init__(self,base,weights_path):
+        self.inputs =  tf.keras.layers.Input(shape=(64,64,3))
+        self.conv1 =  tf.keras.layers.Conv2D(16,4,activation="relu",padding="same",input_shape=(64,64,3))(self.inputs)
+        self.conv2 =  tf.keras.layers.Conv2D(16,2,activation="relu",)(self.conv1)
+        self.pool1 =  tf.keras.layers.MaxPool2D()(self.conv2)
+        self.conv3 =  tf.keras.layers.Conv2D(32,3,activation="relu",)(self.pool1)
+        self.conv4 =  tf.keras.layers.Conv2D(32,3,activation="relu",)(self.conv3)
+        self.pool2 =  tf.keras.layers.MaxPool2D()(self.conv4)
+        self.flat =  tf.keras.layers.Flatten()(self.pool2)
+        self.out =  tf.keras.layers.Dense(128)(self.flat)
+
+        self.embedding_layer =  tf.keras.models.Model(self.inputs,self.out)
+        self.embedding_layer.load_weights(weights_path)
+        
+        self.base = self.embedding_layer.predict(base.reshape(1,64,64,3))
+        self.scoring_layer = RMSEScore(base=self.base)(self.out)
+        self.model =  tf.keras.models.Model(self.inputs,self.scoring_layer)   
+        
+    def __call__(self,inputs):
+        assert len(inputs.shape) > 2,"Input Must Have Dimentions Greater Then 2"
+        return self.model.predict(inputs.reshape(-1,64,64,3))
